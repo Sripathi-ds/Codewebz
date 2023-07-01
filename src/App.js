1259f7098cb5aabc5b66f2ffda60396105e792b7
sripathi-ds <sripathiids@gmail.com>
2023-07-01 22:30:11 +0530

    home component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import Home from './component/home';
+import Style from './assets/css/style_main.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={`${Style['']}`}>
       <Home />
     </div>
   );
--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,11 @@
+import Style from '../assets/css/home.module.css';
+import Container from '@mui/material/Container';
+
+
 const Home = () =>{
 return (
     <div>
-        
+        <h3 className='text-center'>Order your website, Start your journey from today</h3>
     </div>
 )
 }
