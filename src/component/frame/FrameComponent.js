7ed9de910b5bae7602be04d57dbb098245f532f1
sripathi-ds <sripathiids@gmail.com>
2023-07-02 16:16:47 +0530

    Frame component

--- src/component/frame/FrameComponent.js
+++ src/component/frame/FrameComponent.js
@@ -1,10 +1,10 @@
-import { Box } from '@mui/material'
+import { Box, Hidden } from '@mui/material'
 import Style from '../../assets/css/frame/FrameComponent.module.css'
 
 const FrameComponent = () =>{
     return(
-        <Box >
- 
+        <Box className={`${Style['frame_component']}`} borderRadius={3} border={3} overflow={'hidden'} borderColor={'#fff'} bgcolor={'#fff'}>
+            <Box className={`${Style['frame_fill']}`} ></Box>
         </Box>
     )
 }
--- src/component/home.js
+++ src/component/home.js
@@ -1,24 +1,27 @@
 import { Box, Grid, Typography } from '@mui/material';
-
-import Style from '../assets/css/home.module.css';
+import FrameComponent from './frame/FrameComponent';
 import Container from '@mui/material/Container';
+
+
 
 
 
 const Home = () => {
     return (
         <Container>
-            <Box sx={{ mt: 3 }} height={'100vh'}>
-                <Typography className='text-center' color={'white'} fontSize={25} fontFamily={'Merienda'}>Order your website , Start your journey from today</Typography>
-                <Box sx={{mt:3}}>
-                    <Grid container spacing={2}>
-                        <Grid xs={6}>
-                            
+            <Box sx={{ mt: 3 }} height={'calc(100vh - 110px)'} display={'flex'} alignItems={'center'}>
+                <Box width={'100%'}>
+                    <Typography className='text-center' color={'white'} fontSize={25} fontFamily={'Merienda'}>Order your website , Start your journey from today</Typography>
+                    <Box sx={{ mt: 5 }}>
+                        <Grid container colGap={2}>
+                            <Grid item xs={12} md={6}>
+                                <FrameComponent />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <FrameComponent />
+                            </Grid>
                         </Grid>
-                        <Grid xs={6}>
-                        abc
-                        </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
 
             </Box>
