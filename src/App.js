d23ecca686763866bc7129b4419a7e1317efa8be
sripathi-ds <sripathiids@gmail.com>
2023-07-04 08:03:57 +0530

    router & navbar added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import NavbarComponent from './component/header/NavbarComponent';
 import Style from './assets/css/style_main.module.css';
-import { BrowserRouter, Link, Route, Routes, useRoutes } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, Routes ,RouterProvider} from 'react-router-dom';
 import Home from './pages/home'
 import About from './pages/about'
 import AllThemes from './pages/Alltheme';
@@ -10,11 +10,37 @@ import Order from './pages/Order';
 import Contact from './pages/Contact-us';
 import Services from './pages/services';
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home/>,
+//   },
+//   {
+//     path: "/about/",
+//     element: <About />,
+//   },
+//   {
+//     path: "/allTheme/",
+//     element: <AllThemes />,
+//   },
+//   {
+//     path: "/services/",
+//     element: <Services />,
+//   },
+//   {
+//     path: "/order/",
+//     element: <Order />,
+//   },
+//   {
+//     path: "/contact/",
+//     element: <Contact />,
+//   },
+// ]);
+
 function App() {
    return (
     <div>
       <NavbarComponent />
-      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/about/' element={<About />}></Route>
@@ -23,9 +49,8 @@ function App() {
           <Route path='/order/' element={<Order />}></Route>
           <Route path='/contact/' element={<Contact />}></Route>
         </Routes>
-      </BrowserRouter>
-
-
+        {/* <BrowserRouter> <RouterProvider router={router} /> </BrowserRouter> */}
+        
     </div>
   );
 }
--- src/component/header/NavbarComponent.js
+++ src/component/header/NavbarComponent.js
@@ -1,8 +1,151 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Style from '../../assets/css/header/Navbar.module.css'
+import { Link } from 'react-router-dom';
+
+
+const pages = ['Products', 'Pricing', 'Blog'];
+
+const NavPage = [{ name: 'All Themes', navLink: '/AllThemes' },
+{ name: 'About us', navLink: '/about/' },
+{ name: 'Services', navLink: '/services/' },
+{ name: 'Order Now', navLink: '/order/' },
+{ name: 'Contact us', navLink: '/contact/' },
+]
+
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <div>
-        
-        </div>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'Merienda',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Company
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'Merienda',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Company
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+                        {NavPage.map(({name , navLink}) => (
+                            <Link
+                                key={name}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, px:3,  display: 'block' }}
+                                to={navLink} 
+                                underline='none'
+                                color='#fff'
+                                className={`${Style['nav_link']}`}
+                            >
+                                {name}
+                            </Link>
+                        ))}
+                    </Box>
+
+                    <Box >
+                        <Button variant='contained'>Log in / Sign up</Button>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 export default ResponsiveAppBar;
